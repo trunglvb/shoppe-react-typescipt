8a699aba3574263066f321043f621353bde8ac67
trunglvb <levubaotrung0311@gmail.com>
2024-01-21 20:06:17 +0700

    fix input number

--- frontend/src/components/InputNumber/InputNumber.tsx
+++ frontend/src/components/InputNumber/InputNumber.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, InputHTMLAttributes } from 'react'
+import { forwardRef, InputHTMLAttributes, useState } from 'react'
 
 export interface InputNumberProps extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
@@ -13,19 +13,22 @@ const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(function Inpu
     classNameInput = 'p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm',
     classNameError,
     onChange,
+    value = '',
     ...rest
   },
   ref
 ) {
+  const [localValue, setLocalValue] = useState<string>('')
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target
     if (/^\d+$/.test(value) || value === '') {
       onChange && onChange(event)
+      setLocalValue(value)
     }
   }
   return (
     <div className={className}>
-      <input className={classNameInput} onChange={handleChange} {...rest} ref={ref} />
+      <input className={classNameInput} value={value || localValue} onChange={handleChange} {...rest} ref={ref} />
       <div className={classNameError}>{errorMessage}</div>
     </div>
   )
---
+++ frontend/src/components/InputV2/index.ts
@@ -0,0 +1,2 @@
+import InputV2 from './InputV2'
+export default InputV2
