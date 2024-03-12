df34a109429e4e0cd31d5f6b5b35cdf12a937f51
trunglvb <levubaotrung0311@gmail.com>
2024-03-12 21:30:11 +0700

    [shopee]: ui profile page

--- frontend/src/pages/User/layouts/components/UserSideNav/UserSideNav.tsx
+++ frontend/src/pages/User/layouts/components/UserSideNav/UserSideNav.tsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import path from 'src/constants/path'
 
 const UserSideNav = () => {
-  return <div>UserSideNav</div>
+  return (
+    <>
+      <div className='flex items-center border-b border-b-gray-200 py-4'>
+        <Link to={path.profile} className='h-10 w-10 flex-shrink-0 overflow-hidden rounded-full'>
+          <img
+            className='h-full w-full rounded-full object-cover'
+            src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg'
+            alt='avatar'
+          />
+        </Link>
+        <div className='flex-grow pl-4'>
+          <div className='mb-1 truncate font-semibold text-gray-600'>lvbtrung</div>
+          <Link to={path.profile} className='flex items-center gap-1 capitalize text-gray-500'>
+            <svg
+              width={10}
+              height={10}
+              viewBox='0 0 12 12'
+              xmlns='http://www.w3.org/2000/svg'
+              style={{ marginRight: 4 }}
+            >
+              <path
+                d='M8.54 0L6.987 1.56l3.46 3.48L12 3.48M0 8.52l.073 3.428L3.46 12l6.21-6.18-3.46-3.48'
+                fill='#9B9B9B'
+                fillRule='evenodd'
+              />
+            </svg>
+            Sửa hồ sơ
+          </Link>
+        </div>
+      </div>
+      <div className='mt-7'>
+        <Link to={path.profile} className='mt-4 flex items-center capitalize text-orange transition-colors '>
+          <div className='mr-3 h-[22px] w-[22px]'>
+            <img src='https://cf.shopee.vn/file/ba61750a46794d8847c3f463c5e71cc4' alt='' className='h-full w-full' />
+          </div>
+          Tài khoản của tôi
+        </Link>
+        <Link to={path.profile} className='mt-4 flex items-center capitalize text-orange transition-colors'>
+          <div className='mr-3 h-[22px] w-[22px]'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth='1.5'
+              stroke='currentColor'
+              className='text-[#405da7]'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z'
+              />
+            </svg>
+          </div>
+          Đổi mật khẩu
+        </Link>
+        <Link to={path.profile} className='mt-4 flex items-center capitalize text-orange transition-colors'>
+          <div className='mr-3 h-[22px] w-[22px]'>
+            <img src='https://cf.shopee.vn/file/f0049e9df4e536bc3e7f140d071e9078' alt='' className='h-full w-full' />
+          </div>
+          Đơn mua
+        </Link>
+      </div>
+    </>
+  )
 }
 
 export default UserSideNav
--- frontend/src/pages/User/pages/Profile/Profile.tsx
+++ frontend/src/pages/User/pages/Profile/Profile.tsx
@@ -1,7 +1,80 @@
-import React from 'react'
+import Input from 'src/components/Input'
 
 const Profile = () => {
-  return <div>Profile</div>
+  return (
+    <>
+      <div className='border-b border-b-gray-200 py-3'>
+        <div className='text-lg font-medium capitalize text-gray-900'>Hồ sơ của tôi</div>
+        <div className='mt-1 text-sm text-gray-700'>Quản lý thông tin hồ sơ để bảo mật tài khoản</div>
+      </div>
+      <div className='mt-6 flex flex-col-reverse md:flex-row md:items-start'>
+        <form className='flex-grow md:mt-0 md:pr-12'>
+          <div className='mt-3 flex flex-wrap md:mt-0'>
+            <div className='w-[20%] truncate text-right capitalize'>Email</div>
+            <div className='w-[80%] pl-5'>
+              <div className=' text-gray-400'>trunglvb.hust@gmail.com</div>
+            </div>
+          </div>
+          <div className='mt-3 flex flex-wrap'>
+            <div className='w-[20%] truncate pt-3 text-right capitalize'>Tên</div>
+            <div className='w-[80%] pl-5'>
+              <Input classNameInput='w-full border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-wrap'>
+            <div className='w-[20%] truncate pt-3 text-right capitalize'>Số điện thoại</div>
+            <div className='w-[80%] pl-5'>
+              <Input classNameInput='w-full border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-wrap'>
+            <div className='w-[20%] truncate pt-3 text-right capitalize'>Địa chỉ</div>
+            <div className='w-[80%] pl-5'>
+              <Input classNameInput='w-full border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-wrap'>
+            <div className='w-[20%] truncate pt-3 text-right capitalize'>Ngày sinh</div>
+            <div className='w-[80%] pl-5'>
+              <div className='flex justify-between gap-3'>
+                <select className='h-10 w-full rounded-sm border border-black/10'>
+                  <option disabled>Ngày</option>
+                </select>
+                <select className='h-10 w-full rounded-sm border border-black/10'>
+                  <option disabled>Tháng</option>
+                </select>
+                <select className='h-10 w-full rounded-sm border border-black/10'>
+                  <option disabled>Năm</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </form>
+        <div className='mt-2 flex flex-shrink-0 justify-center md:mt-0 md:w-72 md:border-l md:border-l-gray-200'>
+          <div className='flex flex-col items-center'>
+            <div className='my-5 h-24 w-24'>
+              <img
+                className='h-full w-full rounded-full object-cover'
+                src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg'
+                alt='avatar'
+              />
+            </div>
+            <input type='file' className='hidden' accept='.jpg, .jpeg, .png' />
+            <button
+              className='flex h-10 items-center justify-end rounded-sm border bg-white px-6 text-sm text-gray-600 shadow-sm hover:bg-gray-200'
+              type='button'
+            >
+              Chọn ảnh
+            </button>
+            <div className='mt-3 text-gray-400'>
+              <div>Dụng lượng file tối đa 1 MB.</div>
+              <div>Định dạng:.JPEG, .PNG</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Profile
